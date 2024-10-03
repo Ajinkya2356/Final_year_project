43c30bb33f0cdd62acef438265921c87d6d98e0e
Ajinkya Jagtap <115774348+Ajinkya2356@users.noreply.github.com>
2024-10-04 01:00:58 +0530

    Final Status Screen

--- src/components/Checkpoints.tsx
+++ src/components/Checkpoints.tsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect, useRef } from 'react';
 import Webcam from 'react-webcam';
+import FinalStatus from './FinalStatus';
+import { useNavigate } from 'react-router-dom';
 
 const Checkpoints: React.FC = () => {
     const [activeStep, setActiveStep] = useState(0);
@@ -9,15 +11,21 @@ const Checkpoints: React.FC = () => {
     const webcamRef = useRef<Webcam>(null);
     const trueImageUrl = 'https://rishabh.co.in/uploads/product/RISH_LM_1340_(1).png';
     const steps = ['S1', 'S2', 'S3', 'S4', 'S5'];
+    const navigate = useNavigate();
+    const startLoadingTimer = () => {
+        setLoading(true);
+        setStatus(null);
+        const timer = setTimeout(() => {
+            const isPass = Math.random() > 0.5;
+            setStatus(isPass ? 'pass' : 'fail');
+            setLoading(false);
+        }, 5000);
+
+        return () => clearTimeout(timer);
+    };
 
     useEffect(() => {
-        if (activeStep === 1) {
-            setLoading(true);
-            setTimeout(() => {
-                setStatus('pass'); // or 'fail'
-                setLoading(false);
-            }, 2000);
-        }
+        startLoadingTimer();
     }, [activeStep]);
 
     const capture = () => {
@@ -27,6 +35,7 @@ const Checkpoints: React.FC = () => {
 
     const retry = () => {
         setCapturedImage(null);
+        startLoadingTimer();
     };
 
     const handleNext = () => {
@@ -36,6 +45,19 @@ const Checkpoints: React.FC = () => {
     const handleBack = () => {
         setActiveStep((prevStep) => prevStep - 1);
     };
+
+    if (activeStep == steps.length - 1) {
+        return (
+            <FinalStatus
+                meterDetails="ID: 12345"
+                meterImage={trueImageUrl}
+                screenStatuses={steps.map((step, index) => ({ name: step, status: index < activeStep ? 'pass' : 'fail' }))}
+                accuracy={Math.floor(Math.random() * 100)}
+                onRetry={() => setActiveStep(0)}
+                onNext={() => navigate('/list')}
+            />
+        );
+    }
 
     return (
         <div className="p-4 text-center flex flex-col flex-1 w-full">
@@ -67,84 +89,99 @@ const Checkpoints: React.FC = () => {
                     </div>
                 </div>
             </div>
-            <div className="flex flex-col mb-4 border">
-                {activeStep === 0 && (
-                    <>
-                        <div className='flex items-center justify-between border flex-1'>
-                            <div className="flex gap-20 w-full p-4 border flex-1">
-                                <div className="flex flex-col items-center">
-                                    <h4 className="text-lg mb-2">Preview Screen</h4>
-                                    <Webcam
-                                        audio={false}
-                                        ref={webcamRef}
-                                        screenshotFormat="image/jpeg"
-                                        className="h-96 w-96 mb-4 border border-gray-300 rounded-lg"
-                                    />
-                                </div>
-                                <div className="flex flex-col items-center">
-                                    <h4 className="text-lg mb-2">True Image</h4>
-                                    <img src={trueImageUrl} alt="True Image" className="h-96 w-96 mb-4 border border-gray-300 rounded-lg" />
-                                </div>
+            <div className="flex flex-col mb-4">
+                <>
+                    <div className='flex items-center justify-between flex-1'>
+                        <div className="flex gap-20 w-full p-5 flex-1 bg-gray-800 rounded-md
+                            justify-between ">
+                            <div className="flex flex-col items-center">
+                                <h4 className="text-lg mb-2">Preview Screen</h4>
+                                {
+                                    capturedImage ? (
+                                        <img src={capturedImage} alt="Captured Image" className="h-96 w-96 mb-4 border border-gray-300 rounded-lg" />
+                                    ) : (
+                                        <Webcam
+                                            audio={false}
+                                            ref={webcamRef}
+                                            screenshotFormat="image/jpeg"
+                                            className="h-96 w-96 mb-4 border border-gray-300 rounded-lg"
+                                        />
+                                    )
+                                }
 
                             </div>
-                            <div className="flex flex-col mt-4 h-full bg-gray-800 border">
-                                <button
-                                    onClick={retry}
-                                    className="bg-red-500 text-white py-2 px-4 rounded hover:bg-red-600 transition duration-300"
-                                >
-                                    Retry
-                                </button>
-                                <button
-                                    onClick={capture}
-                                    className="bg-green-500 text-white py-2 px-4 rounded hover:bg-green-600 transition duration-300"
-                                >
-                                    Capture
-                                </button>
+                            <div className="flex flex-col items-center">
+                                <h4 className="text-lg mb-2">True Image</h4>
+                                <img src={trueImageUrl} alt="True Image" className="h-96 w-96 mb-4 border border-gray-300 rounded-lg" />
+                            </div>
+
+                        </div>
+                        <div className='flex flex-col gap-20 pt-3 pb-3'>
+                            <div className='flex flex-col gap-10 mx-5'>
+                                {loading ? (
+                                    <div className="flex items-center justify-center p-5">
+                                        <svg className="animate-spin h-20 w-20 text-gray-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                                            <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
+                                            <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8v8H4z"></path>
+                                        </svg>
+                                    </div>
+                                ) : (
+                                    <div className="flex flex-col items-center">
+                                        {status === 'pass' ? (
+                                            <div className="flex flex-col items-center">
+                                                <svg className="h-20 w-20 text-green-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+                                                    <path strokeLinecap="round" strokeLinejoin="round" d="M5 13l4 4L19 7" />
+                                                </svg>
+                                                <p className="text-green-500 text-2xl mt-2">Pass</p>
+                                            </div>
+                                        ) : (
+                                            <div className="flex flex-col items-center">
+                                                <svg className="h-20 w-20 text-red-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+                                                    <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+                                                </svg>
+                                                <p className="text-red-500 text-2xl mt-2">Fail</p>
+                                            </div>
+                                        )}
+                                    </div>
+                                )}
+                            </div>
+                            <div className="flex flex-col gap-10 mx-5 ">
+                                <div className='flex gap-10 justify-between'>
+                                    <button
+                                        onClick={retry}
+                                        className="bg-red-500 text-white py-2 px-4 rounded hover:bg-red-600 transition duration-300 h-1/2 w-1/2"
+                                    >
+                                        Retry
+                                    </button>
+                                    <button
+                                        onClick={capture}
+                                        className="bg-green-500 text-white py-2 px-4 rounded hover:bg-green-600 transition duration-300 h-1/2 w-1/2"
+                                    >
+                                        Capture
+                                    </button>
+                                </div>
+                                <div className='flex gap-10 justify-between'>
+                                    <button
+                                        onClick={handleBack}
+                                        disabled={activeStep === 0 || loading}
+                                        className={`bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 transition duration-300 ${activeStep === 0 ? 'opacity-50 cursor-not-allowed' : ''} h-1/2 w-1/2`}
+                                    >
+                                        Previous
+                                    </button>
+                                    <button
+                                        onClick={handleNext}
+                                        disabled={loading}
+                                        className={`bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 transition duration-300 h-1/2 w-1/2`}
+                                    >
+                                        {
+                                            activeStep == steps.length - 1 ? "Finish" : "Next"
+                                        }
+                                    </button>
+                                </div>
                             </div>
                         </div>
-                        {capturedImage && (
-                            <div className="flex flex-col items-center mb-4">
-                                <h4 className="text-lg mb-2">Captured Image</h4>
-                                <img src={capturedImage} alt="Captured" className="h-72 w-72 mb-4 border border-gray-300" />
-                            </div>
-                        )}
-
-                    </>
-                )}
-
-                {activeStep === 1 && (
-                    <div className="flex justify-center items-center h-72 border border-gray-300 mb-4">
-                        {loading ? (
-                            <div className="flex justify-center items-center">
-                                <svg className="animate-spin h-8 w-8 text-gray-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-                                    <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
-                                    <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8v8H4z"></path>
-                                </svg>
-                            </div>
-                        ) : (
-                            <h4 className={`text-4xl ${status === 'pass' ? 'text-green-500' : 'text-red-500'}`}>
-                                {status === 'pass' ? 'Pass' : 'Fail'}
-                            </h4>
-                        )}
                     </div>
-                )}
-            </div>
-
-            <div className="flex justify-between mt-4">
-                <button
-                    onClick={handleBack}
-                    disabled={activeStep === 0}
-                    className={`bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 transition duration-300 ${activeStep === 0 ? 'opacity-50 cursor-not-allowed' : ''}`}
-                >
-                    Previous
-                </button>
-                <button
-                    onClick={handleNext}
-                    disabled={activeStep === steps.length - 1}
-                    className={`bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 transition duration-300 ${activeStep === steps.length - 1 ? 'opacity-50 cursor-not-allowed' : ''}`}
-                >
-                    Next
-                </button>
+                </>
             </div>
         </div>
     );
